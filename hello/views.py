563c24fe20911dd5810122cc10c4c433a724f1bd
SevenAcademy <admin@sevenacademy.com>
2019-04-10 12:49:59 -0400

    viewpy

--- hello/views.py
+++ hello/views.py
@@ -1,19 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import requests
 
-from .models import Greeting
-
-# Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    print(request)
+    return HttpResponse('<pre>' + r.text + '</pre>')
