377c253e0522a898028ea7c4c9d22905173435f7
SevenAcademy <admin@sevenacademy.com>
2019-04-10 13:12:18 -0400

    os.path.abspath(os.curdir)

--- hello/views.py
+++ hello/views.py
@@ -7,13 +7,15 @@ import uuid
 import time
 import hashlib
 import base64
+import os
 from ecdsa import SigningKey
 from ecdsa.util import sigencode_der
 
 def index(request):
     #payload = request.POST["signature"]
     payload = "com.edokicademy.montessoriacademy_K93Z7MT4A2_subscriptionForAYear_50ME_Montessori_1f83d05c-83d1-497e-afad-5d0c4c14bc75_1554909568808"
-    with open("cert.der", "rb") as myfile:
+    curdir = os.path.abspath(os.curdir)
+    with open(os.path.join(curdir,"cert.der"), "rb") as myfile:
         der = myfile.read()
         signing_key = SigningKey.from_der(der)
         signature = signing_key.sign(payload.encode("utf-8"),hashfunc=hashlib.sha256,sigencode=sigencode_der)
