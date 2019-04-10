e6364cd6f5b240f547adbdbd7a647332cea17cb4
SevenAcademy <admin@sevenacademy.com>
2019-04-10 14:19:21 -0400

    os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'staticfiles')

--- hello/views.py
+++ hello/views.py
@@ -15,7 +15,7 @@ from ecdsa.util import sigencode_der
 def index(request):
     #payload = request.POST["signature"]
     payload = "com.edokicademy.montessoriacademy_K93Z7MT4A2_subscriptionForAYear_50ME_Montessori_1f83d05c-83d1-497e-afad-5d0c4c14bc75_1554909568808"
-    with open(os.path.join(settings.STATIC_ROOT,"./cert.der"), "rb") as myfile:
+    with open(os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'staticfiles'),"./cert.der"), "rb") as myfile:
         der = myfile.read()
         signing_key = SigningKey.from_der(der)
         signature = signing_key.sign(payload.encode("utf-8"),hashfunc=hashlib.sha256,sigencode=sigencode_der)
