bb5fe321b680d6eade6297d05a0d10c6c4eaac57
SevenAcademy <admin@sevenacademy.com>
2019-04-10 14:15:53 -0400

    settings

--- hello/views.py
+++ hello/views.py
@@ -14,7 +14,7 @@ from .models import Greeting
 def index(request):
     #payload = request.POST["signature"]
     payload = "com.edokicademy.montessoriacademy_K93Z7MT4A2_subscriptionForAYear_50ME_Montessori_1f83d05c-83d1-497e-afad-5d0c4c14bc75_1554909568808"
-    with open(os.path.join(STATIC_ROOT,"./cert.der"), "rb") as myfile:
+    with open(os.path.join(settings.STATIC_ROOT,"./cert.der"), "rb") as myfile:
         der = myfile.read()
         signing_key = SigningKey.from_der(der)
         signature = signing_key.sign(payload.encode("utf-8"),hashfunc=hashlib.sha256,sigencode=sigencode_der)
