8ae0c6f9666d86196faa4b7434eebff5affdde3e
SevenAcademy <admin@sevenacademy.com>
2019-04-11 08:31:13 -0400

    jaime ça pitonner

--- hello/views.py
+++ hello/views.py
@@ -13,14 +13,17 @@ from ecdsa import SigningKey
 from ecdsa.util import sigencode_der
 
 def index(request):
-    payload = request.POST["signature"]
-    with open(os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'staticfiles'),"./cert.der"), "rb") as myfile:
-        der = myfile.read()
-        signing_key = SigningKey.from_der(der)
-        signature = signing_key.sign(payload.encode("utf-8"),hashfunc=hashlib.sha256,sigencode=sigencode_der)
-        encoded_signature = base64.b64encode(signature)
-        encoded_signature = str(encoded_signature, "utf-8")
-        return HttpResponse(encoded_signature)
+    if request.POST["signature"] is None:
+        return HttpResponse("Hey, manque une signature en POST");
+    else:
+        payload = request.POST["signature"]
+        with open(os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'staticfiles'),"./cert.der"), "rb") as myfile:
+            der = myfile.read()
+            signing_key = SigningKey.from_der(der)
+            signature = signing_key.sign(payload.encode("utf-8"),hashfunc=hashlib.sha256,sigencode=sigencode_der)
+            encoded_signature = base64.b64encode(signature)
+            encoded_signature = str(encoded_signature, "utf-8")
+            return HttpResponse(encoded_signature)
 
 def db(request):
 
